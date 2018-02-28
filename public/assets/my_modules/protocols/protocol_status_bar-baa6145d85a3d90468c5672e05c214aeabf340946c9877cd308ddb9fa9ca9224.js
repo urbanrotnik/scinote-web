2a23eb9f5f08ff0c551c489899137596f91df9df
zmagod <zmago_devetak@yahoo.com>
2018-02-28 16:49:09 +0100

    remove precompiled assets

--- public/assets/my_modules/protocols/protocol_status_bar-baa6145d85a3d90468c5672e05c214aeabf340946c9877cd308ddb9fa9ca9224.js
+++
@@ -1,6 +0,0 @@
-$(function(){
-  $('body').tooltip( {selector: '[data-toggle=tooltip]'} );
-});
-$(function () {
-  $('[data-toggle="popover"]').popover();
-});
--- public/assets/repositories/edit-0e1214675ca84ed8a08e5f86d890f395df6fa38dd29ed887885bb6e3cfd36036.js
+++
@@ -1,7 +0,0 @@
-(function() {
-  'use strict';
-
-  $('.delete-repo-option').initializeModal('#delete-repo-modal');
-  $('.rename-repo-option').initializeModal('#rename-repo-modal');
-  $('.copy-repo-option').initializeModal('#copy-repo-modal');
-})();
